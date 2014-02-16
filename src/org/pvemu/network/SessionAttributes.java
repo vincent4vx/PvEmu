965140fe8d6361aa035c2a99ecc68675549dbb19
vincent4vx <quatrevieux.vincent@gmail.com>
2014-02-16 21:28:35 +0100

    ADD session attributes enum

---
+++ src/org/pvemu/network/SessionAttributes.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pvemu.network;
+
+import org.apache.mina.core.session.IoSession;
+import org.pvemu.game.objects.Player;
+import org.pvemu.models.Account;
+
+/**
+ *
+ * @author Vincent Quatrevieux <quatrevieux.vincent@gmail.com>
+ */
+public class SessionAttributes<T> {
+    static final public SessionAttributes<String> CONNEXION_KEY = new SessionAttributes<>();
+    static final public SessionAttributes<Account> ACCOUNT = new SessionAttributes<>();
+    static final public SessionAttributes<Player> PLAYER = new SessionAttributes<>();
+    
+    /**
+     * Constructeur privée
+     */
+    private SessionAttributes(){}
+    
+    /**
+     * Retourne la valeur stocké dans la session
+     * @param session la session courrante
+     * @return l'objet stocké, null si inexistant
+     */
+    public T getValue(IoSession session){
+        return (T)session.getAttribute(this);
+    }
+    
+    /**
+     * Stocke une valeur dans la session
+     * @param value la valeur à stocker
+     * @param session la session courante
+     */
+    public void setValue(T value, IoSession session){
+        session.setAttribute(this, value);
+    }
+}
